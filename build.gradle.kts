d93c2c3c5cfb7f329476ead37728b44b4f9c18ee
Ben <128577990+bencoronard@users.noreply.github.com>
2025-07-05 12:20:36 +0700

    Remove toml file for centralized dependency management

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+  `java-library`
+}
+
+group = 'dev.hireben.demo'
+version = '1.0.0-SNAPSHOT'
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  testImplementation("org.junit.jupiter:junit-jupiter:5.12.1")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.named<Test>("test") {
+  useJUnitPlatform()
+}
