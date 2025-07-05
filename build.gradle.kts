850d1000feacb2f486ff5099e09a854a1e101513
Ben <128577990+bencoronard@users.noreply.github.com>
2025-07-05 15:09:52 +0700

    Install Lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ repositories {
 }
 
 dependencies {
+  compileOnly("org.projectlombok:lombok:1.18.38")
+  annotationProcessor("org.projectlombok:lombok:1.18.38")
   testImplementation("org.junit.jupiter:junit-jupiter:5.12.1")
   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
