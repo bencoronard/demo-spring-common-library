913b1f3a8fbb3a72ca567867b5538800ab41bca6
Ben <128577990+bencoronard@users.noreply.github.com>
2025-07-06 14:31:26 +0700

    Add Spring framework web dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 }
 
 dependencies {
+  implementation("org.springframework:spring-web:6.2.8")
   compileOnly("org.projectlombok:lombok:1.18.38")
   annotationProcessor("org.projectlombok:lombok:1.18.38")
   testImplementation("org.junit.jupiter:junit-jupiter:5.12.1")
