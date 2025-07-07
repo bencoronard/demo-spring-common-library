5390e8596ef6dc3a6d6e7d8b73942a41e8d6ecb4
Ben <128577990+bencoronard@users.noreply.github.com>
2025-07-07 10:36:37 +0700

    Suppress warnings

--- src/test/java/dev/hireben/demo/common/CommonLibraryTests.java
+++ src/test/java/dev/hireben/demo/common/CommonLibraryTests.java
@@ -6,16 +6,20 @@ import org.junit.jupiter.api.Test;
 
 import dev.hireben.demo.common.domain.dto.Paginable;
 import dev.hireben.demo.common.domain.dto.Paginated;
+import dev.hireben.demo.common.presentation.configuration.ErrorAttributesConfig;
 
+@SuppressWarnings({ "unlikely-arg-type", "unused" })
 class CommonLibraryTests {
 
   // ---------------------------------------------------------------------------//
   // Methods
   // ---------------------------------------------------------------------------//
 
-  @SuppressWarnings("unlikely-arg-type")
   @Test
-  void testOne() {
+  void testMain() {
+
+    ErrorAttributesConfig conf = new ErrorAttributesConfig(null);
+
     Paginable paginable = Paginable.builder().build();
     Paginated<Object> paginated = Paginated.builder().build();
     assertTrue(!paginable.equals(paginated), "Hello, JUnit!");
