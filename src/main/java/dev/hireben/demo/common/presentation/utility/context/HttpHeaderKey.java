510b8ee866c0b1e685d3c0dba49c54cea9c1c390
Ben <128577990+bencoronard@users.noreply.github.com>
2025-07-07 22:12:44 +0700

    Remove granular user header

--- src/main/java/dev/hireben/demo/common/presentation/utility/context/HttpHeaderKey.java
+++ src/main/java/dev/hireben/demo/common/presentation/utility/context/HttpHeaderKey.java
@@ -12,7 +12,4 @@ public class HttpHeaderKey {
   public final String API_KEY = "X-Api-Key";
   public final String TRACE_ID = "X-Trace-Id";
 
-  public final String USER_ID = "X-User-Id";
-  public final String USER_TENANT = "X-User-Tenant";
-
 }
