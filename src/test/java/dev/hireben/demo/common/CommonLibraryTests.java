55a2b4f6f790806a36b7e7bf2ddcc5090f293bc1
Ben <128577990+bencoronard@users.noreply.github.com>
2025-07-07 22:13:08 +0700

    Remove removed concepts

--- src/test/java/dev/hireben/demo/common/CommonLibraryTests.java
+++ src/test/java/dev/hireben/demo/common/CommonLibraryTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import dev.hireben.demo.common.domain.dto.Paginable;
 import dev.hireben.demo.common.domain.dto.Paginated;
-import dev.hireben.demo.common.presentation.configuration.ErrorAttributesConfig;
 
 @SuppressWarnings({ "unlikely-arg-type", "unused" })
 class CommonLibraryTests {
@@ -17,9 +16,6 @@ class CommonLibraryTests {
 
   @Test
   void testMain() {
-
-    ErrorAttributesConfig conf = new ErrorAttributesConfig(null);
-
     Paginable paginable = Paginable.builder().build();
     Paginated<Object> paginated = Paginated.builder().build();
     assertTrue(!paginable.equals(paginated), "Hello, JUnit!");
