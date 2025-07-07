07ab78e6abb68900060e960869ea621b7a1ed025
Ben <128577990+bencoronard@users.noreply.github.com>
2025-07-07 22:11:37 +0700

    Add jakarta dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,10 @@ repositories {
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot:3.5.3")
+  // implementation("org.springframework.boot:spring-boot:3.5.3")
   implementation("org.springframework:spring-webmvc:6.2.8")
+  compileOnly("jakarta.servlet:jakarta.servlet-api:6.1.0")
+  implementation("jakarta.validation:jakarta.validation-api:3.1.1")
   implementation("com.fasterxml.jackson.core:jackson-databind:2.19.1")
   implementation("org.slf4j:slf4j-api:2.0.17")
   compileOnly("org.projectlombok:lombok:1.18.38")
