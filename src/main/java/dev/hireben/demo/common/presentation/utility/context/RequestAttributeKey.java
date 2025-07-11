c5c6f36477802faac13b3c358afca7b855b75b78
Ben <128577990+bencoronard@users.noreply.github.com>
2025-07-11 23:47:18 +0700

    Add request context classes

---
+++ src/main/java/dev/hireben/demo/common/presentation/utility/context/RequestAttributeKey.java
@@ -0,0 +1,15 @@
+package dev.hireben.demo.common.presentation.utility.context;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class RequestAttributeKey {
+
+  // ---------------------------------------------------------------------------//
+  // Fields
+  // ---------------------------------------------------------------------------//
+
+  public final String ERROR_DETAIL = "servlet.error.detail";
+  public final String TRACE_ID = "servlet.request.trace.id";
+
+}
