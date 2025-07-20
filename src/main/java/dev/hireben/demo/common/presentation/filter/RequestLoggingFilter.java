470d87db6154d8e5a8f3383b9e78389b3b4eb6a2
Ben <128577990+bencoronard@users.noreply.github.com>
2025-07-20 22:41:33 +0700

    Create RequestLoggingFilter.java

---
+++ src/main/java/dev/hireben/demo/common/presentation/filter/RequestLoggingFilter.java
@@ -0,0 +1,10 @@
+package dev.hireben.demo.common.presentation.filter;
+
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RequestLoggingFilter extends CommonsRequestLoggingFilter {
+
+}
