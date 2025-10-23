0c5996f8e644460f7d248d05b177e527db7596f2
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-10-23 21:32:45 +0700

    Init JwtUtil unit tests

--- src/test/java/dev/hireben/demo/common/utility/JwtUtilTests.java
+++ src/test/java/dev/hireben/demo/common/utility/JwtUtilTests.java
@@ -1,5 +1,57 @@
 package dev.hireben.demo.common.utility;
 
+import java.security.KeyPair;
+import javax.crypto.SecretKey;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import io.jsonwebtoken.Jwts;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class JwtUtilTests {
+
+  private final String ISSUER = this.getClass().getName();
+  private JwtUtil withAsymmKeys;
+  private JwtUtil withSymmKey;
+  private JwtUtil withoutKey;
+
+  // =============================================================================
+
+  @BeforeAll
+  void setup() {
+    // Generate RSA key pair
+    KeyPair keyPair = Jwts.SIG.RS256.keyPair().build();
+
+    // Generate AES symmetric key
+    SecretKey symmKey = Jwts.SIG.HS256.key().build();
+
+    // Construct JwtUtil instances
+    withAsymmKeys = new JwtUtil(keyPair.getPrivate(), keyPair.getPublic(), ISSUER);
+    withSymmKey = new JwtUtil(symmKey, symmKey, ISSUER);
+    withoutKey = new JwtUtil(null, null, ISSUER);
+  }
+
+  // =============================================================================
+
+  @Test
+  void testIssueTokenWithAsymmetricKeys() {
+    withAsymmKeys.issueToken(ISSUER, null, null, null, null);
+  }
+
+  // -----------------------------------------------------------------------------
+
+  @Test
+  void testIssueTokenWithSymmetricKey() {
+    withSymmKey.issueToken(ISSUER, null, null, null, null);
+  }
+
+  // -----------------------------------------------------------------------------
+
+  @Test
+  void testIssueTokenWithoutKey() {
+    withoutKey.issueToken(ISSUER, null, null, null, null);
+  }
 
 }
