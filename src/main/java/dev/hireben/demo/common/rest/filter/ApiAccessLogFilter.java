9801ce6e50181b0a7754b117d466a86b32901b4b
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-10-28 17:19:11 +0700

    Implement API access log filter

--- src/main/java/dev/hireben/demo/common/rest/filter/ApiAccessLogFilter.java
+++ src/main/java/dev/hireben/demo/common/rest/filter/ApiAccessLogFilter.java
@@ -1,22 +1,46 @@
 package dev.hireben.demo.common.rest.filter;
 
-import org.springframework.web.filter.AbstractRequestLoggingFilter;
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-public final class ApiAccessLogFilter extends AbstractRequestLoggingFilter {
-
-  @Override
-  protected void beforeRequest(HttpServletRequest request, String message) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'beforeRequest'");
-  }
-
-  // -----------------------------------------------------------------------------
+public final class ApiAccessLogFilter extends OncePerRequestFilter {
 
   @Override
-  protected void afterRequest(HttpServletRequest request, String message) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'afterRequest'");
+  protected void doFilterInternal(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+
+    if (!isAsyncDispatch(request)) {
+      logger.info(String.format(
+          "[id: %s] received %s %s %s",
+          request.getRequestId(),
+          request.getMethod(),
+          request.getRequestURI(),
+          request.getProtocol()));
+    }
+
+    long start = System.currentTimeMillis();
+
+    try {
+      filterChain.doFilter(request, response);
+    } finally {
+      if (!isAsyncStarted(request)) {
+        return;
+      }
+
+      logger.info(String.format(
+          "[id: %s] responded %d in %dms",
+          request.getRequestId(),
+          response.getStatus(),
+          System.currentTimeMillis() - start));
+    }
   }
 
 }
