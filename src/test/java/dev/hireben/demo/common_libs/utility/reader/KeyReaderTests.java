7323f464e83bb21d30b726520643400c78c02f2a
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-11-02 12:54:12 +0700

    Init key reader tests

--- src/test/java/dev/hireben/demo/common_libs/utility/reader/KeyReaderTests.java
+++ src/test/java/dev/hireben/demo/common_libs/utility/reader/KeyReaderTests.java
@@ -5,10 +5,8 @@ import org.junit.jupiter.api.TestInstance;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 final class KeyReaderTests {
 
-  private final String RSA_PRIVATE_KEY_PKCS8 = """
-      """;
-  private final String RSA_PUBLIC_KEY_X509 = """
-      """;
+  private final String RSA_PRIVATE_KEY_PKCS8 = new String("rsa-private-pkcs8.pem");
+  private final String RSA_PUBLIC_KEY_X509 = new String("rsa-public-x509.pem");
 
   // =============================================================================
 
